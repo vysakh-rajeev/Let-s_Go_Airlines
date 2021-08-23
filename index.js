f75c9ea64b2661491675ba8495c9489830a5cdd7
vysakh-rajeev <89316227+vysakh-rajeev@users.noreply.github.com>
2021-08-23 10:01:43 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const db = require('./services/mysql_db');
+const PORT = 3000; // defining the PORT to 3000
+const db = require('./services/mysql_db'); // define db 
 var bodyParser = require('body-parser')
 
 app.use(express.json());
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views'); // setting to frontend folder
+app.engine('html', require('ejs').renderFile); // activate template engine ejs used by Node.js
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); // access images and css from public folder
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// Display the Airport location to select origin and destination from database table
 
 app.get('/', (req,res) => {
     db.query('select * from airport_location', function(err, result, fields){
@@ -33,12 +35,14 @@ app.get('/', (req,res) => {
     });
 })
 
+// Display all the available flights after validating with the collected orgin, detsination and date of journey
+
 app.get('/search/result', (req,res) => {
     let origin = req.query.origin;
     let destination = req.query.destination;
     let dateofjourney = req.query.dateofjourney;
     let numberofpassengers = req.query.numberofpassengers;
-
+// Querying 
     db.query('select * from flight_details where Source="'+origin+'" AND Destination="'+destination+'" AND DOJ="'+dateofjourney+'"', function(err, result, fields){
         if (err)
         {
@@ -50,24 +54,26 @@ app.get('/search/result', (req,res) => {
             for (var i in result)
             {
                 var people = result[i];
-                if(people.Seat_Availability>=numberofpassengers){
+                if(people.Seat_Availability>=numberofpassengers){ // check if seats are available in the flight
                     people.numberofpassengers = numberofpassengers;
                     arr.push(people);
                 }
             }
-            // console.log(arr[0].Seat_Availability);
+            
             res.render('search_results.html', {flights : arr});
         }
     });
-    // res.send('done');
+
 })
 
+// Collect the flight id and no of passengers using get method
 app.get('/book', (req,res) => {
     var fid = req.query.fid;
     var noof = req.query.no;
     res.render('book.html', {fid : fid, noof : noof});
 })
 
+// Display all the necessary details after successful booking with unique booking id and seat numbers
 app.post('/book/flight', urlencodedParser, async(req,res) => {
     var fid = req.body.fid;
     var noof = req.body.numberofpass;
@@ -121,6 +127,7 @@ app.post('/book/flight', urlencodedParser, async(req,res) => {
                                 seats += temp
                                 pass += fname+' '+lname+', '
                             }
+                            // Add the details entered by the passengers to the passenger_information table
                             db.query('INSERT INTO passenger_infomation (First_Name, Last_Name, Age, Gender, Email, Phone, FLIGHT_ID, BOOKING_ID) VALUES ("'+fname+'","'+lname+'","'+age+'","'+gender+'","'+email+'","'+phone+'","'+fid+'","'+bookid+'")', function(err, result, fields){
                                 if (err)
                                 {
@@ -130,7 +137,8 @@ app.post('/book/flight', urlencodedParser, async(req,res) => {
                                 }
                             });
                         }
-                        db.query('update flight_details set Seat_Availability=Seat_Availability-'+noof+' where Flight_ID="'+fid+'"', function(err, result, fields){
+                            // Update the availability of seats bu subtracting with the no. of passengers booked
+                            db.query('update flight_details set Seat_Availability=Seat_Availability-'+noof+' where Flight_ID="'+fid+'"', function(err, result, fields){
                             if (err)
                             {
                                 console.log(err);
@@ -140,22 +148,26 @@ app.post('/book/flight', urlencodedParser, async(req,res) => {
                         });
                     }else{
                         error = true;
-                        reason = 'Tickets Sold Out'
+                        reason = 'Tickets Sold Out' // Display reason in case of parallel booking ans resulting in 0 availability of seats
                     }
                 if(error){
+                    // Load the failure page in case of Unsuccessful booking
                     res.render('failed.html', { reason: reason});
                 }else{
+                    // Display the success page with all necessary information and details
                     res.render('success.html',{fid: fid, source: source, destination: destination, at: at, dt: dt, seats: seats, pass: pass, bookid: bookid  });
                 }
             }
         });     
 })
 
+// Making PORT to listen 
 app.listen(
     PORT,
     () => console.log(`App is running on port : ${PORT}`)
 )
 
+// Funtion definition to generate unique booking id for successful booking
 function randomString(length) {
     return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 }
